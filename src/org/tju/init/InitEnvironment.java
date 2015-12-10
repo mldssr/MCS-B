67335dff14dad475508d4cdf4d87ca474b1ee6b5
yuanzichao <yuanzichao90@126.com>
2015-12-10 11:30:55 +0800

    update configure files

---
+++ src/org/tju/init/InitEnvironment.java
@@ -0,0 +1,13 @@
+package org.tju.init;
+
+/**
+ * Name: InitEnvironment
+ * Description: Initialization Simulation Environment
+ * 
+ * @author yuan
+ *
+ * @date 2015年12月10日 上午11:13:15
+ */
+public class InitEnvironment {
+
+}
