39ef3f47d15f24798d5784ee10b538aaec37e4f8
yuanzichao <yuanzichao90@126.com>
2015-12-14 14:03:42 +0800

    Add replacement strategy of data exchange

---
+++ src/org/tju/scheduler/ReplacementStrategy.java
@@ -0,0 +1,115 @@
+package org.tju.scheduler;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.tju.bean.BlockInfo;
+import org.tju.bean.DiskInfo;
+
+/**
+ * @author yuan
+ *
+ * @date 2015年12月14日 上午11:17:20
+ */
+public class ReplacementStrategy {
+	
+	
+	//Data Disk ====>> SSD Replacement Strategy
+	public static void DD2SSD(DiskInfo dataDisk, DiskInfo SSDDisk, int blockId){
+		
+		BlockInfo block = dataDisk.getBlockList().get(blockId-1);
+		SSDDisk.getBlockList().put(blockId-1, block);
+		SSDDisk.setBlockAmount(SSDDisk.getBlockAmount()+1);
+		
+		block = dataDisk.getBlockList().get(blockId);
+		SSDDisk.getBlockList().put(blockId, block);
+		SSDDisk.setBlockAmount(SSDDisk.getBlockAmount()+1);
+
+		block = dataDisk.getBlockList().get(blockId+1);
+		SSDDisk.getBlockList().put(blockId+1, block);	
+		SSDDisk.setBlockAmount(SSDDisk.getBlockAmount()+1);
+
+	}
+	
+	
+	//Data Disk ====>> Cache Disk Replacement Strategy
+	public static void DD2Cache(DiskInfo dataDisk, DiskInfo cacheDisk, int blockId){
+		
+		BlockInfo block = dataDisk.getBlockList().get(blockId-1);
+		cacheDisk.getBlockList().put(blockId-1, block);
+		cacheDisk.setBlockAmount(cacheDisk.getBlockAmount()+1);
+
+		block = dataDisk.getBlockList().get(blockId);
+		cacheDisk.getBlockList().put(blockId, block);
+		cacheDisk.setBlockAmount(cacheDisk.getBlockAmount()+1);
+	
+		block = dataDisk.getBlockList().get(blockId+1);
+		cacheDisk.getBlockList().put(blockId+1, block);	
+		cacheDisk.setBlockAmount(cacheDisk.getBlockAmount()+1);
+
+	}
+	
+
+	//Cache Disk ====>> SSD Replacement Strategy
+	public static void Cache2SSD(DiskInfo cacheDisk, DiskInfo SSDDisk, double highPriorityTh){
+		
+		HashMap<Integer, BlockInfo> highPriorityBlock = new HashMap<Integer, BlockInfo>();
+		
+		List<Integer> highPriorityBlockId = new LinkedList<Integer>();
+		
+		Iterator<Entry<Integer, BlockInfo>> iter = cacheDisk.getBlockList().entrySet().iterator();
+		
+		while (iter.hasNext()){
+			Entry<Integer, BlockInfo> entry = iter.next();
+			BlockInfo block = entry.getValue();
+
+			if(block.getPriority() > highPriorityTh){
+				highPriorityBlock.put(block.getBlockId(), block);
+				highPriorityBlockId.add(block.getBlockId());
+			}		
+		}
+		
+		SSDDisk.getBlockList().putAll(highPriorityBlock);
+		SSDDisk.setBlockAmount(SSDDisk.getBlockAmount()+highPriorityBlock.size());
+
+		for(int i=0; i<highPriorityBlockId.size(); i++){
+			cacheDisk.getBlockList().remove(highPriorityBlockId.get(i));
+		}
+		cacheDisk.setBlockAmount(cacheDisk.getBlockAmount()-highPriorityBlockId.size());
+		
+	}
+	
+	
+	//Cache Disk ====>>Cache Disk Replacement Strategy
+	public static void HDDtoHDDReplacement(DiskInfo[] cacheDisks, HashMap<Integer,BlockInfo> totalBlocks, int blockInCache){
+		
+		//Clear BolckList of each cache disks && close disk
+		for (int i=0; i<cacheDisks.length; i++) {
+			cacheDisks[i].getBlockList().clear();
+			cacheDisks[i].setDiskState(0);
+		}
+			
+		Iterator<Entry<Integer,BlockInfo>> iterTotal = totalBlocks.entrySet().iterator();
+		
+		int cacheId = 0;
+		
+		while (iterTotal.hasNext()){
+			Entry<Integer,BlockInfo> entry = iterTotal.next();
+			
+			if(cacheDisks[cacheId].getBlockAmount() <= blockInCache){
+				cacheDisks[cacheId].setDiskState(1);
+				cacheDisks[cacheId].getBlockList().put(entry.getKey(), entry.getValue());
+				cacheDisks[cacheId].setBlockAmount(cacheDisks[cacheId].getBlockAmount()+1);
+			}else {
+				cacheDisks[++cacheId].setDiskState(1);
+				cacheDisks[cacheId].getBlockList().put(entry.getKey(), entry.getValue());
+				cacheDisks[cacheId].setBlockAmount(cacheDisks[cacheId].getBlockAmount()+1);
+			}
+		}
+	
+	}
+	
+}
