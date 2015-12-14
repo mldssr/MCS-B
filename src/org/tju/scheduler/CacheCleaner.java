fc12cb7c8419bea498b9a3d324bff9816426362e
yuanzichao <yuanzichao90@126.com>
2015-12-14 15:05:34 +0800

    Add function of cache clean: Cache Cleaner

---
+++ src/org/tju/scheduler/CacheCleaner.java
@@ -0,0 +1,57 @@
+package org.tju.scheduler;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.tju.bean.BlockInfo;
+import org.tju.bean.DiskInfo;
+
+
+/**
+ * @author yuan
+ *
+ * @date 2015年12月14日 下午2:17:36
+ */
+public class CacheCleaner {
+	
+	
+	//Clear Cache contains cache disks && SSD
+	public static void clearCache(DiskInfo[] disks, double lowPriorityTh){
+		
+		for(int i=0; i<disks.length; i++){
+			clearBlockList(disks[i], lowPriorityTh);
+		}
+		
+	}
+	
+	
+	//Clear the Block List
+	public static void clearBlockList(DiskInfo disk, double lowPriorityTh){
+		
+		HashMap<Integer, BlockInfo> lowPriorityBlock = new HashMap<Integer, BlockInfo>();
+		
+		List<Integer> lowPriorityBlockId = new LinkedList<Integer>();
+		
+		Iterator<Entry<Integer, BlockInfo>> iter = disk.getBlockList().entrySet().iterator();
+		
+		while (iter.hasNext()){
+			Entry<Integer, BlockInfo> entry = iter.next();
+			BlockInfo block = entry.getValue();
+
+			if(block.getPriority() < lowPriorityTh){
+				lowPriorityBlock.put(block.getBlockId(), block);
+				lowPriorityBlockId.add(block.getBlockId());
+			}		
+		}
+		
+		for(int i=0; i<lowPriorityBlockId.size(); i++){
+			disk.getBlockList().remove(lowPriorityBlockId.get(i));
+		}
+		disk.setBlockAmount(disk.getBlockAmount()-lowPriorityBlockId.size());
+		
+	}
+
+}
